57b449168eb29125919c55ffdc5a8b3cef745b2b
Sofyk12039 <123567097+Sofyk12039@users.noreply.github.com>
2023-12-23 22:33:26 +0200

    23.12.2023

---
+++ script_vipysk/JS.js
@@ -0,0 +1,22 @@
+alert("привіт");
+alert(" привіт");
+
+
+
+document.write("<p>"+"знову привіт)"+"</p>");
+/*alert("tgv");
+alert(
+    8
+    +10
+     +12
+);
+команда нижче повідомляе про помилку
+alert("увага тут буде помилка");
+alert[ 8, 4, 7 ].forEach(alert);
+коментар
+let text= 'hello world', count=5;
+alert(text)
+alert(count)
+
+let name=prompt("привет как тебя зовут? ","наберть ім*я");
+alert("helo dear "+name)*/
